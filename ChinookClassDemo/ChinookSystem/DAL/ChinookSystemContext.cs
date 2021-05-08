7eb16c3a7fd7d6f70e064847f463095b57b1cda7
Don Welch <dwelch@nait.ca>
2021-05-07 18:31:42 -0600

    Created entities (album, artist) creeted DAL context

---
+++ ChinookClassDemo/ChinookSystem/DAL/ChinookSystemContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.Data.Entity;
+using ChinookSystem.Entities;
+#endregion
+
+namespace ChinookSystem.DAL
+{
+    internal class ChinookSystemContext: DbContext
+    {
+        //we need a constructor to pass the connection string name
+        //  as an argument value to EntityFramework DbContext
+        //we want to do this every time we create an instance of this
+        //  context class
+        public ChinookSystemContext() : base("name=ChinookDB")
+        {
+
+        }
+
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Album> Albums { get; set; }
+    }
+}
---
+++ ChinookClassDemo/ChinookSystem/Entities/Artist.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace ChinookSystem.Entities
+{
+    [Table("Artists")]
+    internal class Artist
+    {
+        [Key]
+        public int ArtistId { get; set; }
+
+        [Required(ErrorMessage ="Artist Name is required.")]
+        [StringLength(120, MinimumLength = 1, ErrorMessage ="Artist Name is limited to 120 characters")]
+        public string Name { get; set; }
+    }
+}
