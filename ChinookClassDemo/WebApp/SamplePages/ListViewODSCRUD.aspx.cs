eaade4d5d4f8e5e13add83a788adf2022f1e0264
Don Welch <dwelch@nait.ca>
2021-05-14 18:42:29 -0600

    Listview web page with ODS for CRUD

---
+++ ChinookClassDemo/WebApp/SamplePages/ListViewODSCRUD.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp.SamplePages
+{
+    public partial class ListViewODSCRUD : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        #region Error Handling
+        protected void SelectCheckForException(object sender,
+                            ObjectDataSourceStatusEventArgs e)
+        {
+            MessageUserControl.HandleDataBoundException(e);
+        }
+
+        protected void InsertCheckForException(object sender,
+                    ObjectDataSourceStatusEventArgs e)
+        {
+            if (e.Exception == null)
+            {
+                MessageUserControl.ShowInfo("Process success", "Album has been addded");
+            }
+            else
+            {
+                MessageUserControl.HandleDataBoundException(e);
+            }
+        }
+        protected void UpdateCheckForException(object sender,
+                ObjectDataSourceStatusEventArgs e)
+        {
+            if (e.Exception == null)
+            {
+                MessageUserControl.ShowInfo("Process success", "Album has been update");
+            }
+            else
+            {
+                MessageUserControl.HandleDataBoundException(e);
+            }
+        }
+        protected void DeleteCheckForException(object sender,
+                ObjectDataSourceStatusEventArgs e)
+        {
+            if (e.Exception == null)
+            {
+                MessageUserControl.ShowInfo("Process success", "Album has been removed");
+            }
+            else
+            {
+                MessageUserControl.HandleDataBoundException(e);
+            }
+        }
+        #endregion
+    }
+}
