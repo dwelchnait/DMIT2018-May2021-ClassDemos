9ea73c5d368e33431b9187003c99edec28da0719
Don Welch <dwelch@nait.ca>
2021-06-21 18:25:18 -0600

    install Manage Playlist starter kit

---
+++ ChinookClassDemo/WebApp/SamplePages/ManagePlaylist.aspx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+#region Additonal Namespaces
+using ChinookSystem.BLL;
+using ChinookSystem.ViewModels;
+
+#endregion
+
+namespace WebApp.SamplePages
+{
+    public partial class ManagePlaylist : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            TracksSelectionList.DataSource = null;
+        }
+
+        
+
+        protected void ArtistFetch_Click(object sender, EventArgs e)
+        {
+            
+                //code to go here
+
+          }
+
+
+        protected void GenreFetch_Click(object sender, EventArgs e)
+        {
+
+                //code to go here
+
+        }
+
+        protected void AlbumFetch_Click(object sender, EventArgs e)
+        {
+
+                //code to go here
+
+        }
+
+        protected void PlayListFetch_Click(object sender, EventArgs e)
+        {
+            //code to go here
+ 
+        }
+
+        protected void MoveDown_Click(object sender, EventArgs e)
+        {
+            //code to go here
+ 
+        }
+
+        protected void MoveUp_Click(object sender, EventArgs e)
+        {
+            //code to go here
+ 
+        }
+
+        protected void MoveTrack(int trackid, int tracknumber, string direction)
+        {
+            //call BLL to move track
+ 
+        }
+
+
+        protected void DeleteTrack_Click(object sender, EventArgs e)
+        {
+            //code to go here
+ 
+        }
+
+        protected void TracksSelectionList_ItemCommand(object sender, 
+            ListViewCommandEventArgs e)
+        {
+            //code to go here
+            
+        }
+
+    }
+}
